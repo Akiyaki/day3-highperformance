2d6977d5ff7a430d380b0c97958a124bd7f83b8f
Tomoko Akiyama <tomoko.akiyama@physics.uu.se>
2022-03-09 22:27:22 +0100

    added inheritance

--- classroom.py
+++ classroom.py
@@ -8,7 +8,34 @@ class Person:
         def fullname(self):
                 return self.firstname + " " + self.lastname
 
+#Dabicha = Person("David", "Sandberg")
+#print(Dabicha.fullname())
 
-Dabicha = Person("David", "Sandberg")
-print(Dabicha.fullname())
 
+class Student(Person):
+        def __init__(self, firstname, lastname, physics, math):
+                super().__init__(firstname, lastname)
+                self.physics = physics
+                self.math = math
+
+        def scores(self):
+                return ": Physics - " + str(self.physics) + " " + "Mathematics - " + str(self.math)
+
+        def results(self):
+#                print(self.fullname() + self.scores()) 
+                return self.fullname() + self.scores()
+
+
+#Dabicha = Student("David", "Sandberg", 30, 50)
+#print(Dabicha.fullname() + Dabicha.scores())             
+            
+class Teacher(Person):
+        def __init__(self, firstname, lastname, course):
+                super().__init__(firstname, lastname)
+                self.course = course
+
+        def courseinfo(self):
+                return " "  + self.course
+
+#Dabicha = Teacher("David", "Sandberg", "Physics")
+#print(Dabicha.fullname() + Dabicha.courseinfo())
